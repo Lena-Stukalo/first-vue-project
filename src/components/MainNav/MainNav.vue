5720fe9d72dc7b64dc9d5dc82df04a083b32d0c7
Lena Stukalo <68201887+Lena-Stukalo@users.noreply.github.com>
2026-04-07 23:19:45 +0300

    mainnav

---
+++ src/components/MainNav/MainNav.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="w-full text-sm">
+    <div class="fixed left-0 top-0 h-16 w-full bg-white">
+      <div class="border-brand-gray-1 mx-autos flex h-full flex-none border-b px-8">
+        <a href="/" class="tex-xl flex h-full items-center"> {{ company }} </a>
+        <h2 class="ml-8 flex h-full items-center">
+          Developed by {{ author.firstName }} {{ author.lastName }}
+        </h2>
+      </div>
+    </div>
+  </header>
+</template>
+<script>
+export default {
+  name: 'MainNav',
+  data() {
+    return {
+      company: 'Test Vue Careers',
+      author: {
+        firstName: 'Olena',
+        lastName: ' Stukalo',
+      },
+    }
+  },
+}
+</script>
