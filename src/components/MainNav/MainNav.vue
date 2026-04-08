2db3517eb6a7f58f0e17c302963299bf18930ae4
Lena Stukalo <68201887+Lena-Stukalo@users.noreply.github.com>
2026-04-08 15:53:29 +0300

    test set up

--- src/components/MainNav/MainNav.vue
+++ src/components/MainNav/MainNav.vue
@@ -1,11 +1,8 @@
 <template>
   <header class="w-full text-sm">
     <div class="fixed left-0 top-0 h-16 w-full bg-white">
-      <div class="border-brand-gray-1 mx-autos flex h-full flex-none border-b px-8">
+      <div class="mx-autos flex h-full flex-none border-b border-brand-gray-1 px-8">
         <a href="/" class="tex-xl flex h-full items-center"> {{ company }} </a>
-        <h2 class="ml-8 flex h-full items-center">
-          Developed by {{ author.firstName }} {{ author.lastName }}
-        </h2>
       </div>
     </div>
   </header>
@@ -15,11 +12,7 @@ export default {
   name: 'MainNav',
   data() {
     return {
-      company: 'Test Vue Careers',
-      author: {
-        firstName: 'Olena',
-        lastName: ' Stukalo',
-      },
+      company: 'PetVue Careers',
     }
   },
 }
